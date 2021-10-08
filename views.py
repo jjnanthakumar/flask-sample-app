5a8e9b855ead3bbe9e587be570cd967a49fe494c
Nanthakumar J J <46053893+jjnanthakumar@users.noreply.github.com>
2021-10-08 15:35:55 +0530

    Initial Commit

---
+++ views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+
+from flask import jsonify, request, render_template, redirect, url_for
+
+views = Blueprint(__name__,"views")
+
+@views.route("/")
+def home():
+    return render_template("index.html",name = "Nanthakumar J J")
+
+@views.route("/profile/<user>")
+def profile(user):
+    return render_template("index.html",name = user)
+
+@views.route("/json")
+def getJson():
+    return jsonify({"name": "Nantha"})
+
+@views.route("/redirect")
+def gotopage():
+    return redirect(url_for("views.getJson"))
